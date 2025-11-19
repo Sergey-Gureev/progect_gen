5023e64b425bbb6887f8dc73b2dfc7df754ad127
Sergey Gureev <107174753+Sergey-Gureev@users.noreply.github.com>
2025-11-19 14:40:38 +0200

    Update conftest.py

--- project_gen/my_templates/project/{{cookiecutter.project_slug}}/tests/conftest.py
+++ project_gen/my_templates/project/{{cookiecutter.project_slug}}/tests/conftest.py
@@ -6,9 +6,8 @@ pytest_plugins = ["clients.fixtures"]
 def pytest_configure(config):
     """Ensure consistent working directory for pytest for allure and coverage outputs"""
     current_dir = Path.cwd()
-
     # Change to project root if running from tests directory
-    if current_dir.name == 'tests':
-        project_root = current_dir.parent
+    if 'tests' in str(current_dir):
+        project_root = str(current_dir).split('/test')[0]
         os.chdir(project_root)
         print(f'Changed working directory to: {project_root}')
